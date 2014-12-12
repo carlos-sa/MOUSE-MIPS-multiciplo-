1623817c9c0c720cbf3a09167952eb5457a930e2
carlos-sa <carlos.santoandrea@hotmail.com>
2014-12-12 10:03:11 -0200

    Versao final
    Versao final com a interface entre o mouse e o processador e o programa
    em assembly funcionando.

--- data_memory.vhdl
+++ data_memory.vhdl
@@ -21,8 +21,8 @@ architecture behavioral of data_memory is
 	type data_sequence is array (0 to 2**address_width - 1) of std_logic_vector (data_width - 1 downto 0);  
 -- ModelSim
 	signal data: data_sequence := (
-    0 => "00000000000000000000000000000001",
-		1 => "10101100110001111000110011001010",
+    0 => "00000000000000000000000000000000",
+		1 => "00000000000000000000000000000000",
 		others => (others => 'U'));
 -- Quartus II
 --	signal data: data_sequence;
--- instructions_memory.vhdl
+++ instructions_memory.vhdl
@@ -19,15 +19,34 @@ architecture behavioral of instructions_memory is
 
 	type instructions_sequence is array (0 to length) of std_logic_vector (data_width - 1 downto 0);
 	signal instructions: instructions_sequence :=
-		-- lw $t0, 0($0)
-   (0 => X"8C080000",
-	   -- mtr $a0, $a1
-	 1 => X"54042800",
-		-- sw $a1, 0 ($t0)
-	 2 => X"ad050000",
-	   -- j 1
-	 3 => X"08000001",
-		
+		-- mtr  $a0, a1
+   (0 => X"54042800",
+	-- srl  $t0, $a1, 1
+	1 => X"00054042",
+	-- sll  $t1, $t0, 3
+	3 => X"000848c0",
+	-- sll  $t2, $t0, 1
+	4 => X"00085040",
+	-- add  $t0, $t1, $t2
+	5 => X"012a4020",
+	-- srl  $t1, $a0, 1
+	6 => X"00044842",
+	-- srl  $t2, $t1, 5
+	7 => X"00095142",
+	-- add  $t0, $t0, $t2
+	8 => X"010a4020",
+	-- addi $a3, $0, -1
+	9 => X"2007ffff",
+	-- addi $s1, $0, 0
+	10 => X"20110000",
+	-- sw $s1, 0(s0)
+	11 => x"ae110000",
+	-- sw a3, 0(t0)
+	12 => X"ad070000",
+	-- addi $s0, $t0, 0
+	13 => X"21100000",
+	-- j 0
+	14 => x"08000000",
 		
 		
 		others => (others => 'U'));
